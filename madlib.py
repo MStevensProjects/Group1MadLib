82819f1f6b480bff46d753bcd0d18ad73e2bad2c
Rob Clinger <Robert7595@gmail.com>
2021-10-20 15:55:13 -0400

    Formatting & comment

--- madlib.py
+++ madlib.py
@@ -11,5 +11,20 @@ def index():
         return render_template('index.html')
 
 @app.route("/results", methods=["GET","POST"])
+# Returns the user input to the results form for the madlib to be read by the user.
 def toResults():
-    return render_template('results.html', adjective1 = request.form["adj1"], adjective2 = request.form["adj2"], bird = request.form["bird"], room = request.form["roominhouse"], pastverb = request.form["pastverb"], verb1 = request.form["verb"], relName = request.form["relName"], noun1 = request.form["noun"], liquid = request.form["liquid"], bodypart = request.form["body"], plnoun = request.form["plnoun"], verbing = request.form["verbing"], noun2 = request.form["noun2"])
+    return render_template('results.html',
+                            adjective1 = request.form["adj1"], 
+                            adjective2 = request.form["adj2"], 
+                            bird = request.form["bird"], 
+                            room = request.form["roominhouse"], 
+                            pastverb = request.form["pastverb"], 
+                            verb1 = request.form["verb"], 
+                            relName = request.form["relName"], 
+                            noun1 = request.form["noun"], 
+                            liquid = request.form["liquid"], 
+                            bodypart = request.form["body"], 
+                            plnoun = request.form["plnoun"], 
+                            verbing = request.form["verbing"], 
+                            noun2 = request.form["noun2"]
+                        ) 
