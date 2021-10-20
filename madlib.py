8e75eddae9af871b9109c27caab9357cb86e8ac8
Matthew Stevens <matt.tyler.stevens@gmail.com>
2021-10-20 09:14:04 -0400

    Updated madlib html

--- madlib.py
+++ madlib.py
@@ -8,7 +8,6 @@ def index():
         adjective1 = request.form["adj1"]
         adjective2 = request.form["adj2"]
         bird = request.form["bird"]
-        pastverb = request.form["pastverb"]
         room = request.form["roominhouse"]
         pastverb = request.form["pastverb"]
         verb1 = request.form["verb"]
@@ -19,11 +18,27 @@ def index():
         plnoun = request.form["plnoun"]
         verbing = request.form["verbing"]
         noun2 = request.form["noun2"]
-        print(adjective1, adjective2, bird, pastverb, room, verb1, relName, noun1, liquid, bodypart, plnoun, verbing, noun2)
         return redirect(url_for("results"))
 
     else: 
         return render_template('index.html')
+def printVar():
+    adjective1 = request.form["adj1"]
+    adjective2 = request.form["adj2"]
+    bird = request.form["bird"]
+    room = request.form["roominhouse"]
+    pastverb = request.form["pastverb"]
+    verb1 = request.form["verb"]
+    relName = request.form["relName"]
+    noun1 = request.form["noun"]
+    liquid = request.form["liquid"]
+    bodypart = request.form["body"]
+    plnoun = request.form["plnoun"]
+    verbing = request.form["verbing"]
+    noun2 = request.form["noun2"]
+
+    print(adjective1, adjective2, bird, room, pastverb, verb1, relName, noun1, liquid, bodypart, plnoun, verbing, noun2)
+
 @app.route("/results", methods=["GET","POST"])
 def toResults():
     return render_template('results.html')
