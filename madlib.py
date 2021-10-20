8435ca2b24460f5096044a1deaa1b2a740228ac5
Matthew Stevens <matt.tyler.stevens@gmail.com>
2021-10-20 04:22:17 -0400

    Implemented index and results page of madlib. Submit button is still a WIP.

--- madlib.py
+++ madlib.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-
-def helloWorld():
-    return ("<p>Hello World!</p>")
+def index():   
+    return render_template('index.html')
+def toResults():
+    return render_template('results.html')
