d6b1183bc9551386428d90431e69f9e86dc7b275
Zak Sawyer <gnuhow@gmail.com>
2021-10-23 13:33:06 -0400

    Update madlib-qa.sh

--- jenkins/madlib-qa.sh
+++ jenkins/madlib-qa.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# grep the h1 tag, Trim html tag, trim leading and trailing whitespace
 LINE=$(curl -s madlibs.thegoldenducks.click | grep "<h1>" | sed 's/<.*>\(.*\)<\/.*>/\1/' | sed 's/^[ \t]*//;s/[ \t]*$//')
 #sed -n '/>/,/</p'
 echo "$LINE"
