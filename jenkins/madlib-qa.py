eec5d7b7c75ebe0882478e671a5682f8923bfa45
Zak Sawyer <gnuhow@gmail.com>
2021-10-23 09:12:28 -0400

    Update madlib-qa.py

--- jenkins/madlib-qa.py
+++ jenkins/madlib-qa.py
@@ -11,5 +11,13 @@ chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
 chrome_options.add_argument('--no-sandbox')
 driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
 driver.get("http://madlibs.thegoldenducks.click")
-print(driver.title)
+title = driver.title
+print("title: "+driver.title)
 driver.close()
+
+if driver.title == "Madlib":
+  print "Test Passed!"
+  return 0
+else:
+  print "Test Failed!"
+  return 1
