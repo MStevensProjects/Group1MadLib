1d835ac9102b598c815f560869da1d3bb5ad6310
Zak Sawyer <gnuhow@gmail.com>
2021-10-23 08:37:56 -0400

    Create madlibs-qa.py for jenkins

---
+++ jenkins/madlib-qa.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+  
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+CHROMEDRIVER_PATH = '/usr/bin/chromedriver'
+WINDOW_SIZE = "1920,1080"
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+chrome_options.add_argument('--no-sandbox')
+driver = webdriver.Chrome(chrome_options=chrome_options
+                         )
+driver.get("http://madlibs.thegoldenducks.click")
+print(driver.title)
+driver.close()
