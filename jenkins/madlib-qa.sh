f3d70e91b50998083f3ba6e26e5123a8fc4bed2f
Zak Sawyer <gnuhow@gmail.com>
2021-10-23 12:57:52 -0400

    Update madlib-qa.sh

--- jenkins/madlib-qa.sh
+++ jenkins/madlib-qa.sh
@@ -2,7 +2,7 @@
 LINE=$(curl -s madlibs.thegoldenducks.click | grep "<h1>" | sed 's/<.*>\(.*\)<\/.*>/\1/')
 echo "$LINE"
 TEXT="Thankgiving Dinner Madlib"
-if $LINE -eq "$TEXT";then
+if [$LINE -eq "$TEXT"];then
         echo "H1 found: '$TEXT'"
         exit 0
         else
