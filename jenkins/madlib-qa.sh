1db246a7cd9dc4c474857872d98eff88c5d5c1ba
Zak Sawyer <gnuhow@gmail.com>
2021-10-25 09:59:42 -0400

    Updated thanksgiving grep

--- jenkins/madlib-qa.sh
+++ jenkins/madlib-qa.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # grep the h1 tag, Trim html tag, trim leading and trailing whitespace
-LINE=$(curl -s madlibs.thegoldenducks.click | grep "<h1>" | sed 's/<.*>\(.*\)<\/.*>/\1/' | sed 's/^[ \t\n]*//;s/[ \t\n]*$//' | tr -d "\t\n" )
-echo "$LINE"
-TEXT="Thankgiving Dinner Madlib"
-echo "$TEXT"
-if [ "$LINE" = "Thankgiving Dinner Madlib" ]
+#LINE=$(curl -s madlibs.thegoldenducks.click | grep "<h1>" | sed 's/<.*>\(.*\)<\/.*>/\1/' | sed 's/^[ \t\n]*//;s/[ \t\n]*$//' | tr -d " \n\r\t"  )
+HTML="<h1>Thanksgiving Dinner Madlib</h1></br>"
+LINE=$(echo "$HTML" | grep -o "Thanksgiving Dinner Madlib")
+#echo "Found: $LINE" # | od -x 
+TEXT=$(echo "Thanksgiving Dinner Madlib")
+#echo "Desired: $TEXT" # | od -x
+if [ "$LINE" = "$TEXT" ]
         then
                 echo "H1 found: '$TEXT'"
                 exit 0
