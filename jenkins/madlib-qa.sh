57049d4aac57245ada3e1f61a95035e9e807e532
Zak Sawyer <gnuhow@gmail.com>
2021-10-25 11:23:21 -0400

    Update madlib-qa.sh

--- jenkins/madlib-qa.sh
+++ jenkins/madlib-qa.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
+# Old grep
 # grep the h1 tag, Trim html tag, trim leading and trailing whitespace
 #LINE=$(curl -s madlibs.thegoldenducks.click | grep "<h1>" | sed 's/<.*>\(.*\)<\/.*>/\1/' | sed 's/^[ \t\n]*//;s/[ \t\n]*$//' | tr -d " \n\r\t"  )
-HTML="<h1>Thanksgiving Dinner Madlib</h1></br>"
-LINE=$(echo "$HTML" | grep -o "Thanksgiving Dinner Madlib")
-#echo "Found: $LINE" # | od -x 
-TEXT=$(echo "Thanksgiving Dinner Madlib")
-#echo "Desired: $TEXT" # | od -x
+
+# Test setup
+#HTML="<h1>Thanksgiving Dinner Madlib</h1></br>"
+#LINE=$(echo "$HTML" | grep -o "Thanksgiving Dinner Madlib")
+
+# New grep
+LINE=$(curl -s "http://3.209.236.73" | grep "<h1>" )
+echo "Found: $LINE" # | od -x 
+LINE=$(echo "$LINE" | grep -o "Thanksgiving Dinner MadLib" )
+echo "Grepped: $LINE"
+TEXT=$(echo "Thanksgiving Dinner MadLib")
+echo "Desired: $TEXT" # | od -x
 if [ "$LINE" = "$TEXT" ]
         then
-                echo "H1 found: '$TEXT'"
+                echo "Success: '$TEXT'"
                 exit 0
         else
                 echo "Error, not found: '$TEXT'"
